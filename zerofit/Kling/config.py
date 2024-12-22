532cc96478e4b4cb2a7dcf1cb69576476c428336
lsj5657 <109152587+lsj5657@users.noreply.github.com>
2024-12-22 20:44:42 +0900

    Delete zerofit directory

--- zerofit/Kling/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-AK = os.getenv("AK")
-SK = os.getenv("SK")
-API_BASE_URL = os.getenv("API_BASE_URL")
-MODEL_NAME = os.getenv("MODEL_NAME")
--- zerofit/SAM2/run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-from config import args
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host=args.host, port=args.port, debug=args.debug)
